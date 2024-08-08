f5c6d74b156f7d2bafd35b99021758c9d5923a2c
Andrés Darín <andres.darin05@gmail.com>
2024-08-08 12:49:55 -0300

    Adjustments

--- src/components/user/ProfilePic.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const ProfilePic = () => {
-    return (
-        <div>ProfilePic</div>
-    )
-}
--- src/routes/Rutas.jsx
+++ src/routes/Rutas.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes, Link } from 'react-router-dom'
-import Login from '../components/user/Login.jsx'
+import { Login } from '../components/user/Login.jsx'
 import Register from '../components/user/Register.jsx';
 import PrivateLayout from "../components/layout/private/PrivateLayout.jsx";
 import PublicLayout from "../components/layout/public/PublicLayout.jsx";
+
 
 
 export const Rutas = () => {
