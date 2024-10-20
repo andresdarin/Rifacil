8fa5d4e3852811d605a0ee0d1edb65ae7a720307
Andrés Darín <andres.darin05@gmail.com>
2024-10-20 19:01:23 -0300

    404 img

--- src/components/layout/private/vendedor/NavVendedor.jsx
+++ src/components/layout/private/vendedor/NavVendedor.jsx
@@ -5,6 +5,15 @@ import { NavLink } from 'react-router-dom'
 const NavVendedor = () => {
     return (
         <li className="menu-list__item">
+            <NavLink to='' className="menu-list__link">
+                <span className="menu-list__title">Estadísticas</span>
+            </NavLink>
+            <NavLink to='' className="menu-list__link">
+                <span className="menu-list__title">Historial Ventas</span>
+            </NavLink>
+            <NavLink to='' className="menu-list__link">
+                <span className="menu-list__title">Vender Rifa</span>
+            </NavLink>
             <NavLink to='/vendedor/logout' className="menu-list__link">
                 <span className="menu-list__title">Cerrar Sesión</span>
             </NavLink>
--- src/routes/Error404.jsx
+++ src/routes/Error404.jsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import useAuth from '../hooks/useAuth';
+import errorImage from '../../src/assets/img/ImgError404.png'
 
 const Error404 = () => {
+
+    useEffect(() => {
+        document.body.style.backgroundImage = "url('/src/assets/img/BackgroundLong.png')";
+        document.body.style.backgroundSize = "cover";
+        document.body.style.backgroundPosition = "center";
+
+        return () => {
+            document.body.style.backgroundImage = '';
+        };
+    }, []);
     const { auth } = useAuth();
 
     const getRedirectPath = () => {
@@ -16,9 +27,12 @@ const Error404 = () => {
     };
 
     return (
-        <div>
-            <h1>Error 404 - Página no encontrada</h1>
-            <Link to={getRedirectPath()}>Volver al inicio</Link>
+        <div className='error-page'>
+            <img src={errorImage} alt='Error 404 - Pagina no encontrada' />
+            <div className="error-page-text">
+                <h2>Parece que nos perdimos...</h2>
+                <Link to={getRedirectPath()}>Volvemos al incio?</Link>
+            </div>
         </div>
     );
 };
