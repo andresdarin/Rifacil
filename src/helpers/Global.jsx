7ee7ff04b7ee48d44ec49351dff45f49118a4a8a
Andrés Darín <andres.darin05@gmail.com>
2025-03-02 17:13:01 -0300

    Amplify connection w EB

--- src/helpers/Global.jsx
+++ src/helpers/Global.jsx
@@ -1,3 +1,7 @@
 export const Global = {
-    url: 'http://localhost:4001/api/'
+    url: import.meta.env.MODE === "development"
+        ? "http://localhost:4001/api/"
+        : "http://rifacil-03-env.eba-4xyc4qtq.sa-east-1.elasticbeanstalk.com/api/"
 };
+//Si ejecutas npm run dev, usará localhost
+//Si está en producción en Amplify, usará la URL de Elastic Beanstalk
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    "process.env.VITE_API_URL": JSON.stringify(process.env.VITE_API_URL)
+  },
   server: {
-    host: '0.0.0.0', // Permite accesos externos
-    port: 5173,      // Mantiene el puerto estándar de Vite
-    strictPort: true // Se asegura de que siempre use este puerto
+    host: "0.0.0.0",
+    port: 5173,
+    strictPort: true
   }
 });
