730f2007ee25700b8701e33aae1d315e47f52f78
Andrés Darín <andres.darin05@gmail.com>
2025-03-02 17:24:21 -0300

    .js en global

--- src/helpers/Global.js
+++ src/helpers/Global.js
@@ -1,7 +1,7 @@
 export const Global = {
     url: import.meta.env.MODE === "development"
         ? "http://localhost:4001/api/"
-        : "http://rifacil-03-env.eba-4xyc4qtq.sa-east-1.elasticbeanstalk.com/api/"
+        : import.meta.env.VITE_API_URL
 };
 //Si ejecutas npm run dev, usará localhost
 //Si está en producción en Amplify, usará la URL de Elastic Beanstalk
